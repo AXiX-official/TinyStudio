2dea3bec6359f2e698819deff2dee6693ac3beb4
AXiX-official <axix@mail.ustc.edu.cn>
2025-10-22 16:16:24 +0800

    feat: improve Texture2D preview speed && fix assetList

--- TinyStudio/Previewer/TexturePreviewer.cs
+++ TinyStudio/Previewer/TexturePreviewer.cs
@@ -1,15 +1,17 @@
+using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
 using TinyStudio.Models;
 using TinyStudio.Views;
 using UnityAsset.NET;
 using UnityAsset.NET.AssetHelper;
-using UnityAsset.NET.AssetHelper.TextureHelper;
 using UnityAsset.NET.Enums;
 using UnityAsset.NET.TypeTreeHelper.PreDefined.Classes;
 
@@ -41,17 +43,22 @@ public class TexturePreviewer : IPreviewer
 
     private async Task LoadTextureAsync(ITexture2D texture2D, AssetManager assetManager, ZoomableImageView zoomableImageView)
     {
-        using var image = await Task.Run(() => assetManager.DecodeTexture2DToImage(texture2D));
+        var (pixelData, width, height) = await Task.Run(() =>
+        {
+            var data = assetManager.DecodeTexture2D(texture2D);
+            FlipVertical(data, texture2D.m_Width, texture2D.m_Height, 4);
+            return (data, texture2D.m_Width, texture2D.m_Height);
+        });
 
-        await LoadImageAsync(image, zoomableImageView);
+        await LoadImageAsync(pixelData, width, height, zoomableImageView);
 
         zoomableImageView.SetInfo($"Width: {texture2D.m_Width}\nHeight: {texture2D.m_Height}\nFormat: {((TextureFormat)texture2D.m_TextureFormat).ToString()}");
     }
     
     private async Task LoadSpriteAsync(ISprite sprite, AssetManager assetManager, ZoomableImageView zoomableImageView)
     {
+        // TODO: Optimize this path to also use WriteableBitmap
         using var image = await Task.Run(() => assetManager.DecodeSpriteToImage(sprite));
-
         await LoadImageAsync(image, zoomableImageView);
     }
     
@@ -63,5 +70,46 @@ public class TexturePreviewer : IPreviewer
 
         var bitmap = new Bitmap(memoryStream);
         zoomableImageView.SetImage(bitmap);
+    }
+    
+    private Task LoadImageAsync(byte[] data, int width, int height, ZoomableImageView zoomableImageView)
+    {
+        if (data.Length == 0)
+        {
+            return Task.CompletedTask;
+        }
+        
+        var writeableBitmap = new WriteableBitmap(
+            new PixelSize(width, height),
+            new Vector(96, 96),
+            PixelFormat.Bgra8888,
+            AlphaFormat.Premul);
+
+        using (var framebuffer = writeableBitmap.Lock())
+        {
+            Marshal.Copy(data, 0, framebuffer.Address, data.Length);
+        }
+        
+        zoomableImageView.SetImage(writeableBitmap);
+        return Task.CompletedTask;
+    }
+    
+    private static void FlipVertical(byte[] data, int width, int height, int bpp)
+    {
+        if (data.Length == 0)
+        {
+            return;
+        }
+        var rowStride = width * bpp;
+        var rowBuffer = new byte[rowStride];
+        for (var y = 0; y < height / 2; y++)
+        {
+            var topRowOffset = y * rowStride;
+            var bottomRowOffset = (height - y - 1) * rowStride;
+            
+            data.AsSpan(topRowOffset, rowStride).CopyTo(rowBuffer.AsSpan(0, rowStride));
+            data.AsSpan(bottomRowOffset, rowStride).CopyTo(data.AsSpan(topRowOffset, rowStride));
+            rowBuffer.AsSpan(0, rowStride).CopyTo(data.AsSpan(bottomRowOffset, rowStride));
+        }
     }
 }
